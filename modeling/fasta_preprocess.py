fa58d44fdcba92b307f9988b560ebea79a549f18
Oran-G <31713576+Oran-G@users.noreply.github.com>
2024-04-11 15:48:55 -0400

    change from esm command line to script

--- modeling/fasta_preprocess.py
+++ modeling/fasta_preprocess.py
@@ -28,7 +28,7 @@ def main(cfg: DictConfig) -> None:
     parser = argparse.ArgumentParser(
                     prog='ESM_embedder',
                     description='Embeds protein sequences using ESM')
-    parser.add_argument('--model_name', type=List[str], 
+    parser.add_argument('--model_name', nargs='*', 
         help='The name of the model to use for embedding', 
         default=['esm2_t48_15B_UR50D', 'esm2_t36_3B_UR50D', 'esm2_t33_650M_UR50D', 'esm2_t30_150M_UR50D']
     )
