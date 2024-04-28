49c3ff0c96a3c2404bdc22a16db51e0b931bf69a
Oran-G <31713576+Oran-G@users.noreply.github.com>
2024-04-28 17:22:22 -0400

    debug test step

--- folding/folding.py
+++ folding/folding.py
@@ -396,8 +396,7 @@ def main(cfg: DictConfig) -> None:
         gradient_clip_val=0.3,
         )
     
-    # try:
-    if True:
+    try:
         #add in support for test-only mode
         print(cfg.model.checkpoint_path)
         print(cfg.model.test_only)
@@ -406,18 +405,20 @@ def main(cfg: DictConfig) -> None:
             model = model.to(torch.device("cuda:0"))
             model.hparams.io.test_embedded = cfg.io.test_embedded
             trainer.test(model, dataloaders=model.test_dataloader())
-            
+            os.mknod(f"/vast/og2114/output_home/runs/slurm_{os.environ['SLURM_JOB_ID']}/test_data.pkl")
             pickle.dump(model.test_data, open(f"/vast/og2114/output_home/runs/slurm_{os.environ['SLURM_JOB_ID']}/test_data.pkl", "wb"))
             wandb.run.summary["test_data"] = wandb.Artifact("test_data", type="dataset")
             wandb.run.summary["test_data"].add_file(f"/vast/og2114/output_home/runs/slurm_{os.environ['SLURM_JOB_ID']}/test_data.pkl", skip_cache=True)
             wandb.run.log_artifact(wandb.run.summary["test_data"])
             return
-    # except:
-    #     pass
+    except:
+        pass
     print('ready to train!')
     trainer.fit(model)
     model = model.to(torch.device("cuda:0"))
     trainer.test(model, dataloaders=model.test_dataloader())
+    os.mknod(f"/vast/og2114/output_home/runs/slurm_{os.environ['SLURM_JOB_ID']}/test_data.pkl")
+
     pickle.dump(model.test_data, open(f"/vast/og2114/output_home/runs/slurm_{os.environ['SLURM_JOB_ID']}/test_data.pkl", "wb"))
     wandb.run.summary["test_data"] = wandb.Artifact("test_data", type="dataset")
     wandb.run.summary["test_data"].add_file(f"/vast/og2114/output_home/runs/slurm_{os.environ['SLURM_JOB_ID']}/test_data.pkl", skip_cache=True)
