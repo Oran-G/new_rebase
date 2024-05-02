f3d69ccd5d2328879b9c3c92ce1f04cd54190279
Oran-G <31713576+Oran-G@users.noreply.github.com>
2024-05-02 10:12:21 -0400

    debug test step

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -278,10 +278,6 @@ def main(cfg: DictConfig) -> None:
 
 
     try:
-    
-        #add in support for test-only mode
-        print(cfg.model.checkpoint_path)
-        print(cfg.model.test_only)
         if cfg.model.checkpoint_path and cfg.model.test_only: 
             print('test-only mode. running test')
             model = model.to(torch.device("cuda:0"))
