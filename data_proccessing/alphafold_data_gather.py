e9e667fadfabcfe57f6f3d4ca4f5352bf5e78398
Oran-G <31713576+Oran-G@users.noreply.github.com>
2024-06-21 10:32:51 -0400

    add alphafold data grabber

---
+++ data_proccessing/alphafold_data_gather.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+@hydra.main(config_path='../configs', config_name='defaults')
+def main(cfg: DictConfig) -> None:
+    df = pd.read_csv('cfg.io.final')
+    filtered_df = df[df['bind'] == 'GATC'].head(20)
+    print(filtered_df)
+
+if __name__ == "__main__":
+    main()
