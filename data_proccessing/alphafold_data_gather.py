2dbf6638efe463296c9187a04332c9ce4bd99ab2
Oran Goodman <og2114@log-3.hpc.nyu.edu>
2024-06-21 11:31:15 -0400

    finished

--- data_proccessing/alphafold_data_gather.py
+++ data_proccessing/alphafold_data_gather.py
@@ -1,10 +1,11 @@
 import pandas as pd
+from omegaconf import DictConfig, OmegaConf
 import hydra
 @hydra.main(config_path='../configs', config_name='defaults')
 def main(cfg: DictConfig) -> None:
-    df = pd.read_csv('cfg.io.final')
+    df = pd.read_csv(cfg.io.final)
     filtered_df = df[df['bind'] == 'GATC'].head(20)
-    print(filtered_df)
+    print(filtered_df.to_string())
 
 if __name__ == "__main__":
     main()
